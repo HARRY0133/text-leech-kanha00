8bdcf959ae1b2d668c55c1bd4cf656af42c347f6
HARRY0133 <excuseboy13@gmail.com>
2024-11-29 00:19:05 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -4,6 +4,6 @@
 
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID    = os.environ.get("API_ID", "24515547")
+API_HASH  = os.environ.get("API_HASH", "0809699a061f02fbbcc1a6c4443547ee")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7873382050:AAFXHVIRXUsbScVKH8g37sERn3WO25K1mTU")
